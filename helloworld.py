349ad3aef81453aa444c29805823817a9590c15b
Phil Blunk <philipblunk@gmail.com>
2023-04-12 16:32:07 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 84957f9106fea9f477ca55f41aa86e224a9d525c
-
 
 main()
